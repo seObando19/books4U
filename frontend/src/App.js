ddad7e25fcea306051f3ab273e1b5c0b330d11f4
edgg72 <edgg72@hotmail.com>
2021-02-23 19:47:37 -0500

    adding background hover effect to signup button and adding app.js modification on imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,49 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom'
 import NavBar from './navBar';
 import HomePage from './Home';
 import BuyPage from './buy';
 import DonatePage from './donate';
 import ContactPage from './contact';
-
-
+import LoginPage from './login';
+import SignupPage from './signup';
+import ForgotPage from './forgot';
 
 
 function App() {
+  const location = useLocation()
   return (
-    <Router>
-      <section className='main'>
-        <NavBar></NavBar>
-        <Switch>
-          <Route exact path='/'>
-            <HomePage></HomePage>
-          </Route>
-          <Route exact path='/buy'>
-            <BuyPage></BuyPage>
-          </Route>
-          <Route exact path='/donate'>
-            <DonatePage></DonatePage>
-          </Route>
-          <Route exact path='/contact'>
-            <ContactPage></ContactPage>
-          </Route>
-        </Switch>
-      </section>
-    </Router>
+
+    <section className='main'>
+      <NavBar></NavBar>
+      <Switch location={location}>
+        <Route exact path='/'>
+          <HomePage></HomePage>
+        </Route>
+        <Route exact path='/buy'>
+          <BuyPage></BuyPage>
+        </Route>
+        <Route exact path='/donate'>
+          <DonatePage></DonatePage>
+        </Route>
+        <Route exact path='/contact'>
+          <ContactPage></ContactPage>
+        </Route>
+        <Route exact path='/login'>
+          <LoginPage></LoginPage>
+        </Route>
+        <Route exact path='/signup'>
+          <SignupPage></SignupPage>
+        </Route>
+        <Route exact path='/forgot'>
+          <ForgotPage></ForgotPage>
+        </Route>
+        <Route path='*'>
+          <div className='not-found'>Not found</div>
+        </Route>
+      </Switch>
+
+    </section>
+
   );
 }
 
