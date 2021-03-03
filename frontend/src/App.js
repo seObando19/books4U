c5c0cf1652743b4cd9438bbe7edc870df29825ab
edgg72 <edgg72@hotmail.com>
2021-03-03 14:06:00 -0500

    adding some changes regarding class based react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NavBar from './navBar';
 import HomePage from './Home';
@@ -7,40 +9,58 @@ import ContactPage from './contact';
 import LoginPage from './login';
 import SignupPage from './signup';
 import ForgotPage from './forgot';
+import './css/index.css';
+import './css/navBar.css';
+import './css/searchBar.css';
+import './css/contact.css';
+import './css/donate.css';
+import './css/login.css';
+import './css/forgot.css'
+import './css/signup.css'
+import './css/buy.css';
 
 
-function App() {
-  return (
-    <section className='main'>
-      <NavBar></NavBar>
-      <Switch >
-        <Route exact path='/'>
-          <HomePage></HomePage>
-        </Route>
-        <Route exact path='/buy'>
-          <BuyPage></BuyPage>
-        </Route>
-        <Route exact path='/donate'>
-          <DonatePage></DonatePage>
-        </Route>
-        <Route exact path='/contact'>
-          <ContactPage></ContactPage>
-        </Route>
-        <Route exact path='/login'>
-          <LoginPage></LoginPage>
-        </Route>
-        <Route exact path='/signup'>
-          <SignupPage></SignupPage>
-        </Route>
-        <Route exact path='/forgot'>
-          <ForgotPage></ForgotPage>
-        </Route>
-        <Route path='*'>
-          <div className='not-found'>Error 404: <p>Page not found</p></div>
-        </Route>
-      </Switch>
-    </section>
-  );
+class App extends Component {
+  componentDidMount() {
+    axios.get('https://603fb4edd952850017605d0b.mockapi.io/api/v1/users')
+      .then(result => {
+        console.log(result.data);
+      }).catch(console.log)
+  }
+
+  render() {
+    return (
+      <section className='main'>
+        <NavBar></NavBar>
+        <Switch >
+          <Route exact path='/'>
+            <HomePage></HomePage>
+          </Route>
+          <Route exact path='/buy'>
+            <BuyPage></BuyPage>
+          </Route>
+          <Route exact path='/donate'>
+            <DonatePage></DonatePage>
+          </Route>
+          <Route exact path='/contact'>
+            <ContactPage></ContactPage>
+          </Route>
+          <Route exact path='/login'>
+            <LoginPage></LoginPage>
+          </Route>
+          <Route exact path='/signup'>
+            <SignupPage></SignupPage>
+          </Route>
+          <Route exact path='/forgot'>
+            <ForgotPage></ForgotPage>
+          </Route>
+          <Route path='*'>
+            <div className='not-found'>Error 404: <p>Page not found</p></div>
+          </Route>
+        </Switch>
+      </section>
+    );
+  }
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, } from 'react-router-dom'
-import './css/index.css';
-import './css/navBar.css';
-import './css/searchBar.css';
-import './css/contact.css';
-import './css/donate.css';
-import './css/login.css';
-import './css/forgot.css'
-import './css/signup.css'
-import './css/buy.css'
+import { BrowserRouter as Router, } from 'react-router-dom';
 import App from './App';
 
 
 ReactDOM.render(
   <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <App />
   </Router>,
   document.getElementById('root')
 );
