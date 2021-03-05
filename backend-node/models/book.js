1901542469544e27eb0bc998154f6b7ec202b3ae
seObando19 <sebastiano-10@hotmail.com>
2021-03-05 14:37:20 -0500

    Updated the schema the book for save document to DB

---
+++ backend-node/models/book.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    author:[{
+        type: mongoose.Types.ObjectId,
+        ref: 'Author',
+        required: false
+    }],
+    genre:[{
+        type: mongoose.Types.ObjectId,
+        ref: 'NAN',
+        required: false
+    }],
+    publisher:[{
+        type: mongoose.Types.ObjectId,
+        ref: 'NAN',
+        required: false
+    }],
+    isbn:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    raiting:{
+        type: Number,
+        required: true
+    },
+    number_pages:{
+        type: Number,
+        required: false
+    },
+    release_year:{
+        type: Number,
+        required: false
+    },
+    created_at:{
+        type: Date,
+        required: false
+    }
+});
+
+const Book = mongoose.model('Book', bookSchema);
+module.exports = Book;
