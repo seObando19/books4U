d2b7f7635d1276233d144b16758d435fa8fcef57
seObando19 <sebastiano-10@hotmail.com>
2021-03-05 14:46:04 -0500

    Added router to app.js

--- backend-node/app.js
+++ backend-node/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bookRouter = require('./routes/book');
+var authorRouter = require('./routes/');
 
 var app = express();
 
@@ -16,5 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/book', bookRouter);
+app.use('/author', authorRouter);
 
 module.exports = app;
