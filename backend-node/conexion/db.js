de0d2bc2bb195624e191614153c65f5758e058a5
seObando19 <sebastiano-10@hotmail.com>
2021-03-07 13:12:29 -0500

    Configure new components to connect to BD

--- backend-node/conexion/db.js
+++ backend-node/conexion/db.js
@@ -3,7 +3,8 @@ const {BOOKS_USER, BOOKS_PASS, BOOKS_DB} = process.env
 
 mongoose.connect(`mongodb+srv://${BOOKS_USER}:${BOOKS_PASS}@cluster0.57q03.mongodb.net/${BOOKS_DB}?retryWrites=true&w=majority`, {
     useUnifiedTopology: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
     .then(db => console.log('Database is connected'))
     .catch(err => console.log(err));
