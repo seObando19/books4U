e2a87dbeaa43f4ef318b0ef691dfacff715d1ca0
Emmanuel Palacio Gaviria <emmanuelpalacio1@hotmail.com>
2021-03-08 08:59:55 -0500

    get, post, delete, put methods were performed

--- backend-node/routes/users.js
+++ backend-node/routes/users.js
@@ -1,9 +1,73 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/', async function (req, res, next){
+    try {
+        const users = await User.find();
+        ;res.status(200).json(users);
+    } catch (error) {
+        res.status(400);
+        ;next(error);
+    }
+});
+
+router.get('/:id', async function (req, res, next){
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id);
+        ;res.status(200).json(user);
+    } catch (error) {
+        res.status(400);
+        ;next(error);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+
+    const user = new User({
+	name: req.body.name,
+	lastname: req.body.lastname,
+	age: req.body.age,
+	email: req.body.email,
+        phone: req.body.phone,
+	username: req.body.username,
+	password: req.body.password,
+	created_at: new Date(),
+	updated_at: new Date()
+    });
+    try {
+	const savedUser = await user.save();
+	res.status(201);
+	res.json(savedUser);
+    } catch (error) {
+	res.status(400);
+	next(error);
+    }
+});
+
+router.delete('/:id', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+	const deleteUser = await User.findByIdAndDelete(id);
+	;res.status(200).json(deleteUser);
+    } catch (error) {
+	res.status(4000);
+	;next(error);
+    }
+
+})
+
+router.put('/:id', async function (req, res, next) {
+    const { id } = req.params;
+    try {
+        const updatedUser = await User.findByIdAndUpdate(id, req.body);
+        ;res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400);
+        ;next(error);
+    }
 });
 
 module.exports = router;
