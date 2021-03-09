e23014c290d5704ccb756799dd4f9ecbcf8dbb92
idelahoz14 <idelahoz14@cuc.edu.co>
2021-03-09 17:46:35 -0500

    Revert "modified variable name"
    This reverts commit 45fb25212f8b17df489e4514bec44e4fea3e8a8a.

--- backend-node/routes/genre.js
+++ backend-node/routes/genre.js
@@ -27,7 +27,7 @@ router.post('/', async function (req, res, next) {
     const genre = new Genre(req.body);
     try {
         const savedGenre = await genre.save();
-        res.status(201).json(savedGenre);
+        res.status(201).json(genres);
     } catch (error) {
         res.status(400);
         next(error);
