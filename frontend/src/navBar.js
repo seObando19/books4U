f9c78578b6c2717682d1e70263d2e9a16c176c6e
seObando19 <sebastiano-10@hotmail.com>
2021-03-15 08:39:24 -0500

    Created new button for login to google

--- frontend/src/navBar.js
+++ frontend/src/navBar.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import GoogleLogin from 'react-google-login';
 
 function NavBar() {
+
+  const responseGoogle = (res) => {
+    console.log(res);
+  }
   return (
     <>
       <nav>
@@ -11,9 +16,18 @@ function NavBar() {
           <li><Link to='/buy' className="nav_button">BUY</Link></li>
           <li><Link to='/donate' className="nav_button">DONATE</Link></li>
           <li><Link to='/contact' className="nav_button">CONTACT</Link></li>
-          <li><div className='login-box'>
+          {/* <li><div className='login-box'>
             <Link to='/login' className="login-text">Log in</Link>
           </div>
+          </li> */}
+          <li>
+          <GoogleLogin
+            clientId="67101201099-e24gt7bsr8ormhrd8kf17avkpfnb8dkg.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
           </li>
         </ul>
         <label htmlFor="check" className="burger">
