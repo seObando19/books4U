7b99a8bc4573e8ffa5544c6f6eef62b6875bb6f4
idelahoz14 <idelahoz14@cuc.edu.co>
2021-03-16 10:03:40 -0500

    adding route and method from backend

--- frontend/src/searchBar.js
+++ frontend/src/searchBar.js
@@ -3,14 +3,14 @@ import React from 'react'
 const SearchBar = () => {
   return (
     <>
-      <div className='search-box'>
-        <input className='search-txt' type='text' name='' placeholder='Search'>
-        </input>
-        <a className='search-btn' href='#'>
-          <i className="fas fa-search"></i>
-        </a>
-      </div>
-
+      <form action="http://localhost:9000/searchBar" method="GET">
+        <div className='search-box'>
+          <input className='search-txt' type='text' name='book-to-search' placeholder='Search' />
+          <a className='search-btn' href="http://localhost:3000/buy">
+            <i className="fas fa-search"></i>
+          </a>
+        </div>
+      </form>
     </>
   );
 }
