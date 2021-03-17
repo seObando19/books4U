09c3f31777678a250d804ae5766af38b0a0c4f4b
idelahoz14 <idelahoz14@cuc.edu.co>
2021-03-17 13:00:29 -0500

    adding new route

--- frontend/src/searchBar.js
+++ frontend/src/searchBar.js
@@ -3,11 +3,11 @@ import React from 'react'
 const SearchBar = () => {
   return (
     <>
-      <form action="http://localhost:9000/searchBar" method="GET">
+      <form action="http://localhost:9000/searchBar" method="POST">
         <div className='search-box'>
           <input className='search-txt' type='text' name='book-to-search' placeholder='Search' />
-          <a className='search-btn' href="http://localhost:3000/buy">
-            <i className="fas fa-search"></i>
+          <a className='search-btn'>
+            <input type="submit" value='Send' id="button-home" />
           </a>
         </div>
       </form>
