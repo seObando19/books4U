bf2eb6384b3d3e3a309d535293a514780603d14e
idelahoz14 <idelahoz14@cuc.edu.co>
2021-03-17 12:58:57 -0500

    new route

--- backend-node/routes/searchBar.js
+++ backend-node/routes/searchBar.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const Book = require('../models/book');
 
-router.get('/', async function (req, res, next) {
-    console.log("hola mundo");
-    res.status(200);
+router.post('/', async function (req, res, next) {
+    let book = req.body;
+    try {
+        // Tomar nombre del libro
+        const book_name = Object.values(book);
+        const searched_book = await Book.find( {name : book_name} );
+        let book_id = searched_book[0]._id;
+        console.log(book_id);
+        res.status(200).redirect('http://localhost:3000/buy');
+    } catch (error) {
+        res.status(400);
+        next(error);
+    }
 });
 
 module.exports = router;
